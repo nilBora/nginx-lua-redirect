d45de6080d3eba5368af44b453f2540fa3b8a075
Nil Borodulia <nborodulia@gmail.com>
2022-02-21 10:58:21 +0300

    add cache

--- public/index.php
+++ public/index.php
@@ -1,8 +1,10 @@
 <?php
 require "./../vendor/autoload.php";
+
 use Jtrw\DAO\DataAccessObject;
 use Jtrw\Redirect\Domain\Service\Redirect;
 use Jtrw\Redirect\Domain\Repository\RedirectRepository;
+use Cache\Adapter\Redis\RedisCachePool;
 
 $CONFIG['db']['dbname'] = '%dbname%';
 $CONFIG['db']['host'] = 'localhost';
@@ -14,8 +16,13 @@ if (file_exists(__DIR__."/local.php")) {
     require_once __DIR__."/local.php";
 }
 
+$client = new \Redis();
+$client->connect('redis_redirect', 6379);
+$pool = new Cache\Adapter\Redis\RedisCachePool($client);
+
+
 try {
-    $service = new Redirect(new RedirectRepository(
+    $service = new Redirect($pool, new RedirectRepository(
         DataAccessObject::factory(new PDO(
             $CONFIG['db']['dsn'],
             $CONFIG['db']['user'],
@@ -26,11 +33,5 @@ try {
     
     $service->doRedirect($url);
 } catch (Exception $exp) {
-    //..
+    //..add to log
 }
-
-
-
-//Redirect logic
-//echo $_SERVER['REQUEST_URI'];
-//echo "PONG";
